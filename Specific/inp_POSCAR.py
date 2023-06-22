99b3dc9233343015b3d8e449732a091a2be24377
NakayamaLab-NITech <102635361+NakayamaLab-NITech@users.noreply.github.com>
2023-06-22 17:27:37 +0900

    Update inp_POSCAR.py

--- Specific/inp_POSCAR.py
+++ Specific/inp_POSCAR.py
@@ -1,6 +1,6 @@
-runtype = "m3g"
+runtype = "m3g" #for static: m3g-static
 ions = [["La", "Sr", "Co", "Fe", "O"]]
 ELEM = [["La", "Sr"], ["Co", "Fe"]]
 savefiles = ["POSCAR", "CONTCAR", "temp_gene"]
 output = "energy"
-#thread = True
+thread = True #import Specific/optm3g.py
